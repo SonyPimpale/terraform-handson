38f1700e80b6b6d10a6ea3fa1c56a6e6a25cc7ca
janvi <janvi@janvis-MBP.broadband>
2024-02-20 22:04:12 +0000

    Changes to Provider.tf files

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-2"
-  access_key = "AKIAW3MEADFIB4NOC5HA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
